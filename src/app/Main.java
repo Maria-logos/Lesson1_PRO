2226408c8a649055dffd6a7f24113780c301007a
Maria <msinukova@gmail.com>
2024-05-06 01:17:43 +0300

    Modify master 1

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,22 +1,16 @@
 package app;
 
 public class Main {
-    private static final double CONV_K = 2.20462  ;
+    private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
-        System.out.println("Converter App. ");
+        System.out.println("App for measure converting. ");
         double kgs = 5;
-        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        double kilos = convPoundsToKgs(pnds);
-        System.out.println("Result is " + pounds + " pounds. " + kilos + " kgs.");
+        System.out.println("Result is " + pounds + " pounds. ");
     }
     private static double convKgsToPounds(double kgs) {
         return kgs * CONV_K;
-    }
-
-    private static double convPoundsToKgs(double pnds) {
-        return pnds / CONV_K;
     }
 
 }
